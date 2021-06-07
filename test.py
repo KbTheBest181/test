b39b0119baaaf4d5e6cd37ac4e9c4809b4cfc2eb
KbTheBest181 <73401085+KbTheBest181@users.noreply.github.com>
2021-06-07 23:25:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import sqlite3
 
 con = sqlite3.connect('testing.db')
-cur = con.curser()
+cur = con.cursor()
 cur.execute("create table test(sno text, name text)")
+cur.execute("insert into test(1, Bhattu)")
+con.commit()
 for i in cur.execute("select * from test"):
   print(i)
