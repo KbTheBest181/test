0516dc83c89be35196d8b1e40daaa81b233e37fd
KbTheBest181 <73401085+KbTheBest181@users.noreply.github.com>
2021-06-07 23:33:59 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-con = sqlite3.connect('testing.db')
+con = sqlite3.connect('DustMiningBot.db')
 cur = con.cursor()
 #cur.execute("create table test(sno text, name text)")
 #cur.execute("insert into test(1, Bhattu)")
 #con.commit()
-for i in cur.execute("select * from test"):
+for i in cur.execute("select * from admins"):
   print(i)
